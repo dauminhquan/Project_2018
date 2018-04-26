0a530f24ff0b077896c81bf6d416471acba6fa15
dauminhquan <dauminhquantlu@gmail.com>
2018-04-26 17:47:41 +0700

    update all chức năng update gửi email

--- resources/views/checkLogin.blade.php
+++
@@ -1,6 +0,0 @@
-<form method="post">
-    <input type="text" name="username">
-    <input type="text" name="password">
-    <input type="text" value="{{csrf_token()}}" name="_token" hidden>
-    <input type="submit" value="submit">
-</form>
